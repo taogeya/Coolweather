08565c56d935d88e4009245eaf1cb9da6bf7cb3f
taogeya <50516972+taogeya@users.noreply.github.com>
2019-05-16 08:34:07 +0800

    First commit

---
+++ app/src/main/java/com/example/admin/coolweather/db/province.java
@@ -0,0 +1,28 @@
+package com.example.admin.coolweather.db;
+
+/**
+ * Created by admin on 2019/5/14.
+ */
+public class Province extends DataSupport {
+    private int id;//每个实体类都有的字段
+    private String provinceName;//记录省份名字
+    private int provinceCode;//记录省的代号
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
